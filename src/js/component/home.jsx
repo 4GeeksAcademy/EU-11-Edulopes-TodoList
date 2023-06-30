42da45c31b7271cf2127fe32f071dca3b8e60259
Eduardo Lopes <129979638+Edulopesp@users.noreply.github.com>
2023-06-30 17:11:42 +0000

    all changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,133 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Update the API with the modified to-do list whenever todos change
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/salomon", {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => {
+        console.log(resp.ok); // true if the response is successful
+        console.log(resp.status); // the status code (e.g., 200, 400, etc.)
+        console.log(resp.text()); // the response as text
+        return resp.json(); // parse the response as JSON and return a promise
+      })
+      .then(data => {
+        console.log(data); // the object received from the server
+      })
+      .catch(error => {
+        console.log(error); // error handling
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/salomon", {
+      method: "PUT",
+      body: JSON.stringify([{ label: "Make the bed", done: false },
+      { label: "Walk for an hour", done: false },
+      { label: "Do the homework", done: false }]),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => {
+        console.log(resp.ok); // true if the response is successful
+        console.log(resp.status); // the status code (e.g., 200, 400, etc.)
+        console.log(resp.text()); // the response as text
+      })
+      .then(data => {
+        console.log(data); // the object received from the server
+      })
+      .catch(error => {
+        console.log(error); // error handling
+      });
+    // Fetch the initial to-do list from the API
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/salomon")
+      .then(resp => resp.json())
+      .then(data => setTodos(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  const addTodo = () => {
+    if (inputValue.trim() === "") return;
+
+    // Create a new to-do object and update the todos state
+    const newTodo = { label: inputValue, done: false };
+    setTodos([...todos, newTodo]);
+
+    // Clear the input field
+    setInputValue("");
+  };
+
+  const deleteTodo = index => {
+    // Remove the selected to-do from the todos state
+    const updatedTodos = todos.filter((_, currentIndex) => index !== currentIndex);
+    setTodos(updatedTodos);
+  };
+
+  const cleanAllTasks = () => {
+    // Delete the entire list from the server and update the todos state
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/salo", {
+      method: "DELETE"
+    })
+      .then(resp => resp.json())
+      .then(data => setTodos([]))
+      .catch(error => console.log(error));
+  };
+
+  return (
+    <div>
+      <h1>todos</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">
+                <input
+                  type="text"
+                  onChange={(e) => setInputValue(e.target.value)}
+                  value={inputValue}
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      addTodo();
+                    }
+                  }}
+                  placeholder="No tasks, add a task"
+                />
+              </li>
+              {todos.map((item, index) => (
+                <li
+                  key={index}
+                  style={{ display: "flex", alignItems: "center" }}
+                  className="list-group-item"
+                >
+                  <span style={{ flexGrow: 1 }}>{item.label}</span>
+                  <i
+                    className="fas fa-times"
+                    onClick={() => deleteTodo(index)}
+                  ></i>
+                </li>
+              ))}
+              <div className="todosLeft">{todos.length} tasks left</div>
+              {todos.length > 0 && (
+                <button className="btn btn-secondary" onClick={cleanAllTasks}>
+                  Clean All Tasks
+                </button>                
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Home;
